b2a713f1f82910d94c1a64b3823f312e3b930e1d
Mitch Kennedy <mitchk@runbox.com>
2022-03-13 21:41:23 +1100

    Starting on Server

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,21 @@
 const std = @import("std");
+const Server = @import("Server.zig");
 
 pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    var serverMode = false;
+
+    var sysargs = try std.process.argsWithAllocator(alloc);
+    while (sysargs.next()) |nxt| {
+        if (std.mem.startsWith(u8, nxt, "--")) {
+            if (std.mem.eql(u8, nxt[2..], "server")) serverMode = true;
+            std.log.info("{s}", .{nxt});
+        }
+    }
+    if (serverMode) Server.runServer();
     std.log.info("All your codebase are belong to us.", .{});
 }
 
